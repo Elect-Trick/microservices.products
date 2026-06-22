62b7431a1da002fcc3153b7f73fcd6a65a748e11
Elect-Trick <snmajenje@gmail.com>
2026-06-22 20:50:46 +0200

    Product
    Add product Enity

---
+++ DomanLayer/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomanLayer.Entities
+{
+    public class Product
+    {
+
+        [Key]
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string Category { get; set; }
+        public double? UnitPrice { get; set; }
+        public int? QuantityInStock { get; set; }
+    }
+}
