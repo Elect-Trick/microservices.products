696412078787c16a66b5aab3969060ce867402ad
Elect-Trick <snmajenje@gmail.com>
2026-06-22 19:17:09 +0200

    Exception Handling
    Added Exception Handling Middleware

--- eCommerceProducts/Program.cs
+++ eCommerceProducts/Program.cs
@@ -1,10 +1,13 @@
 using InfrastructureLayer.DependencyInjection;
 using ApplicationLayer.DependencyInjection;
+using Microsoft.AspNetCore.Diagnostics;
+using eCommerceProducts.Middlewares;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddInfrastructureLayer();
 builder.Services.AddApplicationLayer();
-var app = builder.Build();
 
+var app = builder.Build();
+app.UseExceptionHandlingMiddleware();
 
 
 app.Run();
