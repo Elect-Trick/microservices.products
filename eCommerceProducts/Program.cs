23d869d6322cc2619910691b97b814596c0d3554
Elect-Trick <snmajenje@gmail.com>
2026-06-22 19:23:06 +0200

    start up
    Update start up steps

--- eCommerceProducts/Program.cs
+++ eCommerceProducts/Program.cs
@@ -1,13 +1,17 @@
 using InfrastructureLayer.DependencyInjection;
 using ApplicationLayer.DependencyInjection;
-using Microsoft.AspNetCore.Diagnostics;
 using eCommerceProducts.Middlewares;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddInfrastructureLayer();
 builder.Services.AddApplicationLayer();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 app.UseExceptionHandlingMiddleware();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
 
 
 app.Run();
