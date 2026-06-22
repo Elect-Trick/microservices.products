ae0b7e2a10379a80c961b9838c97d5f4e7543182
Elect-Trick <snmajenje@gmail.com>
2026-06-22 22:31:16 +0200

    SQL server context
    Inject the sql server context

--- InfrastructureLayer/DependencyInjection/DependencyInjection.cs
+++ InfrastructureLayer/DependencyInjection/DependencyInjection.cs
@@ -1,21 +1,27 @@
 ﻿using ApplicationLayer.ServiceContracts;
 using InfrastructureLayer.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using InfrastructureLayer.DatabaseContext;
 
-namespace InfrastructureLayer.DependencyInjection
-{
+namespace InfrastructureLayer.DependencyInjection;
+    
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructureLayer(this IServiceCollection services)
+
+    public static IServiceCollection AddInfrastructureLayer(this IServiceCollection services, IConfiguration configuration, DbContext dbContext)
         {
-           
-            services.AddScoped<IProductService, ProductService>();
+      
+                
+        services.AddScoped<IProductService, ProductService>();
+
+            services.AddDbContext<EfDbContext>(options =>
+           options.UseSqlServer(
+               configuration.GetConnectionString("DefaultConnection"),
+               b => b.MigrationsAssembly(typeof(EfDbContext).Assembly.FullName)
+           )
+       );
             return services;
         }
     }
-}
