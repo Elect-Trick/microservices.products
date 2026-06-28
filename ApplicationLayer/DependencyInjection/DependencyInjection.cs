6e9e09e9fbdee18f726a5d0780eb4701e2653dc4
Elect-Trick <snmajenje@gmail.com>
2026-06-28 17:32:50 +0200

    DI
    Update DI for Application Layer

--- ApplicationLayer/DependencyInjection/DependencyInjection.cs
+++ ApplicationLayer/DependencyInjection/DependencyInjection.cs
@@ -1,4 +1,7 @@
-﻿using DomanLayer.RepositoryContracts;
+﻿using ApplicationLayer.DTOS;
+using ApplicationLayer.Validators;
+using DomanLayer.Entities;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ApplicationLayer.DependencyInjection
@@ -7,7 +10,8 @@ namespace ApplicationLayer.DependencyInjection
     {
         public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
         {
-           
+
+            services.AddScoped<IValidator<ProductDTO>, ProductValidator>();
             return services;
 
         }
