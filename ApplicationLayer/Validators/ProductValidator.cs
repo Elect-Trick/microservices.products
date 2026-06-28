3e439ca7934ee76c1b20c6e97e86f9a0c9915462
Elect-Trick <snmajenje@gmail.com>
2026-06-28 17:31:59 +0200

    Product Validator
    Implement the product validator class

---
+++ ApplicationLayer/Validators/ProductValidator.cs
@@ -0,0 +1,25 @@
+﻿using ApplicationLayer.DTOS;
+using DomanLayer.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationLayer.Validators
+{
+    public class ProductValidator : AbstractValidator<ProductDTO>
+    {
+        public ProductValidator()
+        {
+
+            RuleFor(obj => obj.ProductName).NotEmpty().WithMessage("Product Name Cannot be empty");
+            RuleFor(obj => obj.Category).NotEmpty().WithMessage("Category Cannot be empty");
+            RuleFor(obj => obj.UnitPrice).GreaterThan(0).WithMessage("Price must be a positive value").NotEmpty().WithMessage("Price Cannot be empty");
+            RuleFor(obj => obj.QuantityInStock).GreaterThanOrEqualTo(0).WithMessage("Quantity must be a non-negative value").NotEmpty().WithMessage("Quantity Cannot be empty")
+                .NotNull().WithMessage("Quantity Cannot be null");
+
+        }
+    }
+}
