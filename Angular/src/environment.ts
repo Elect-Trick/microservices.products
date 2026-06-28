896d765a0603291c4ebb891358f3ef0a86ab9e1b
Elect-Trick <snmajenje@gmail.com>
2026-06-28 18:28:25 +0200

    ENV
    Correct env urls

--- Angular/src/environment.ts
+++ Angular/src/environment.ts
@@ -1,7 +1,7 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:7186/api/auth/', // API base URL for development,
-    productsMicroserviceUrl: 'https://localhost:7260/api/products/'
+    apiUrl: 'https://localhost:7255/api/auth/', // API base URL for development,
+    productsMicroserviceUrl: 'https://localhost:7027/api/products/'
   };
   
