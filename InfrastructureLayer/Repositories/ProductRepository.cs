ff06e15e5eda8758e8ced746505d04057eb3f268
Elect-Trick <snmajenje@gmail.com>
2026-06-28 17:32:32 +0200

    Repo Functions
    Implement repo functions that inherit from the product interface

--- InfrastructureLayer/Repositories/ProductRepository.cs
+++ InfrastructureLayer/Repositories/ProductRepository.cs
@@ -1,11 +1,9 @@
 ﻿using DomanLayer.Entities;
 using DomanLayer.RepositoryContracts;
 using InfrastructureLayer.DatabaseContext;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace InfrastructureLayer.Repositories;
     public class ProductRepository : IProductRepository
@@ -15,9 +13,58 @@ namespace InfrastructureLayer.Repositories;
         {
             _context = context; 
         }
-        public  Task<Product?> GetProductById(int id)
+
+    public async Task<Product?> AddProduct(Product product)
+    {
+            Product? existingProduct = _context.Products.FirstOrDefault(p => p.ProductName == product.ProductName);
+        if (existingProduct != null)    {
+            return null;
+        }
+        else
         {
-            Product? product = _context.Products.FirstOrDefault(p => p.ProductID == id);
-            return Task.FromResult(product);
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+            return product;
         }
     }
+
+    public  Task<Product?> GetProductById(int id)
+     {
+            Product? product = _context.Products.FirstOrDefault(p => p.ProductID == id);
+            return Task.FromResult(product);
+     }
+
+    public Task<Product[]> GetAllProducts()
+    {
+        Product[] products = _context.Products.ToArray();
+        return Task.FromResult(products);
+    }
+
+    public async Task<bool> DeleteProduct(Product product)
+    {
+         _context.Products.Remove(product);
+        int rowsAffected = await _context.SaveChangesAsync(); 
+        if(rowsAffected > 0)
+        {
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public async Task<Product[]> GetProductByName(string name)
+    {
+
+       Product[] products = _context.Products
+    .Where(p => EF.Functions.Like(p.ProductName, $"%{name}%"))
+    .ToArray();
+        //Product[] products =  _context.Products.Where(p => p.ProductName.Contains(name)).ToArray();
+        if (products == null || products.Length == 0)
+        {
+            return null;
+        }
+        return products;  
+    }
+
+}
