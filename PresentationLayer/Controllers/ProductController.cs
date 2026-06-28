ce0a6d77c0e4a7e97b0ed522a1cbc6ce3923579f
Elect-Trick <snmajenje@gmail.com>
2026-06-28 17:31:34 +0200

    Product Controller
    Implement missing controller end points

--- PresentationLayer/Controllers/ProductController.cs
+++ PresentationLayer/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
-﻿using ApplicationLayer.ServiceContracts;
+﻿using ApplicationLayer.DTOS;
+using ApplicationLayer.ServiceContracts;
 using DomanLayer.Entities;
+using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 
 
@@ -7,15 +9,25 @@ namespace PresentationLayer.Controllers;
 
     [ApiController]
     [Route("api/[controller]/")]
-    public class ProductController : ControllerBase
+    public class ProductsController : ControllerBase
     {
         private readonly IProductService _productService;
-        public ProductController(IProductService productService)
+        private readonly IValidator<ProductDTO> _productValidator;
+    public ProductsController(IProductService productService, IValidator<ProductDTO> productValidator)
         {
             _productService = productService;
+            _productValidator = productValidator;
         }
 
-        [HttpGet("productId/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+        // Implementation for getting all products
+        Product[] products = await _productService.GetAllProducts();
+            return Ok(products);
+        }
+
+        [HttpGet("search/productId/{id}")]
         public async Task<Product?> GetProductById(int id)
         {
           
@@ -27,5 +39,87 @@ namespace PresentationLayer.Controllers;
            
             return product;
         }
+
+        [HttpGet("search/{productName}")]
+        public async Task<ActionResult<Product?>> GetProductByName(string productName)
+        {
+          
+            Product[] products = await _productService.GetProductByName(productName);
+            if (products == null || products.Length == 0)
+            {
+                return NotFound(new
+                {
+                    Title = "Product not found",
+                    Status = 404,
+                    Message = $"No product found with Name {productName}."
+                });
+        }
+        
+           
+            return Ok(products);
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> AddProduct(ProductDTO productDto)
+        {
+
+
+        var validationResult = await _productValidator.ValidateAsync(productDto);
+
+        if (!validationResult.IsValid)
+        {
+            return BadRequest(new
+            {
+                Title = "Validation failed",
+                Status = 400,
+                Errors = validationResult.Errors
+                             .Select(e => e.ErrorMessage)
+                             .ToList()
+            });
+        }
+
+        Product? product = await _productService.AddProduct(productDto);
+        if (product == null) {
+            return BadRequest(new
+            {
+                Title = "Product Creation failed",
+                Status = 400,
+                Message = "Product with the same name already exists."
+            });
+
+        }
+            return Ok(product);
+        }
+
+    [HttpDelete("delete/{id}")]
+    public async Task<IActionResult> DeleteProduct(int id)
+    {
+        Product? product = await _productService.GetProductById(id);
+        if (product == null)
+        {
+            return NotFound(new
+            {
+                Title = "Product not found",
+                Status = 404,
+                Message = $"No product found with ID {id}."
+            });
+        }
+
+        bool success = await _productService.DeleteProduct(product);
+        if (!success)
+        {
+            return BadRequest(new
+            {
+                Title = "Product Deletion failed",
+                Status = 400,
+                Message = "Failed to delete the product."
+            });
+        }
+        return Ok(new { Message = "Product deleted successfully." });
     }
+}
+
+          
+   
 
