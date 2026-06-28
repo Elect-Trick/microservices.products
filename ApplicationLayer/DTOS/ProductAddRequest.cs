3b55fb30be75597446bad2826bde0c81cbcad915
Elect-Trick <snmajenje@gmail.com>
2026-06-28 17:58:37 +0200

    Product Add Request
    Add Product Add Request DTO

---
+++ ApplicationLayer/DTOS/ProductAddRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationLayer.DTOS
+{
+    public record ProductAddRequest
+    {
+        public string ProductName { get; set; }
+        public string Category { get; set; }
+        public double? UnitPrice { get; set; }
+        public int? QuantityInStock { get; set; }
+    }
+}
